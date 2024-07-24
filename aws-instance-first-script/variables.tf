b93f2f70dd0408890bab2f741dd4951f540c79b2
swapniljunwal <159988036+swapniljunwal@users.noreply.github.com>
2024-07-24 00:39:08 -0700

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -3,11 +3,10 @@ variable "region" {
 }
 
 variable "ami_id" {
-  type = map(string)
+  type = "string"
 
   default = {
     us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
+    
   }
 }
